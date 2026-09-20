0534ba01e881025c5c18e162bf1f7976df23eb96
ashoknarayansmn <ashoknarayansmn@gmail.com>
2026-09-20 09:21:55 +0530

    Deploy site to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// If you deploy to GitHub Pages under a repo subpath, set:
-// base: '/ieee-cs-mbits-webnova/'
 export default defineConfig({
-  base: '/',
+  base: '/IEEEMBITS/',
   plugins: [react(), tailwindcss()],
-});
+})
